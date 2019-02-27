4612e0ec3b7e593ce1d48c21418342ae5df46d55
Yadi <ashleyfreedenberg@gmail.com>
2019-02-27 10:10:07 -0800

    Work from 10:00am 2/27

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+
 var randomNum = Math.floor(Math.random() * 101) + 19;
 
 var wins = 0;
@@ -13,30 +14,29 @@ var crystals = {
 
 $("#blueCrystal").on("click", function () {
     var crystalValBlue = (crystals.blueCrystal);
-    crystalValBlue = parseInt(crystalValBlue);
-    alert(crystalValBlue);
     totalScore += crystalValBlue;
+    console.log(totalScore);
+    $("#totalScore").html(totalScore);
+   
+    if (totalScore === randomNum) {
+        alert("You Won, Keep Playing!");
+        wins++;
+        var totalScore = 0;
+        $("#wins").html(wins);
+    }
+    
+    else if (totalScore > randomNum) {
+        alert("You Lost, Try Again!")
+        losses++
+        var totalScore = 0;
+        $("#losses").html(losses);
+    }
 });
-
-
-if (totalScore === randomNum) {
-    alert("You Won, Keep Playing!");
-    wins++;
-    var totalScore = 0;
-}
-
-else if (totalScore > randomNum) {
-    alert("You Lost, Try Again!")
-    losses++
-    var totalScore = 0;
-}
+console.log(totalScore === randomNum);
 
 
 
 $("#randomNum").html(randomNum);
-$("#wins").html(wins);
-$("#losses").html(losses);
-$("#totalScore").html(totalScore);
 
 $("#blueCrystal").html("<img src='assets/images/BlueCrystal.png'>")
 $("#greenCrystal").html("<img src='assets/images/GreenCrystal.png'>")
@@ -47,3 +47,4 @@ console.log("blueCrystal", crystals.blueCrystal);
 console.log("greenCrystal", crystals.greenCrystal);
 console.log("purpleCrystal", crystals.purpleCrystal);
 console.log("tealCrystal", crystals.tealCrystal);
+
